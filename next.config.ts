d9e244bf4f800f73aff851d1cc18bcc83fdeba35
ttdo19 <thuytrang19072001@gmail.com>
2025-04-18 20:01:06 -0500

    Change config in NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/project-website",
+  output: "export",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
